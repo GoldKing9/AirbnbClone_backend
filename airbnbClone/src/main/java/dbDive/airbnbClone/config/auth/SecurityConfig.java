9187ad53204e0642879d384dc13260a48212d488
PanjunKwon <pndlm1979@naver.com>
2023-08-21 15:50:34 +0900

    [Feat] 10조 CSRF 설정 추가

--- airbnbClone/src/main/java/dbDive/airbnbClone/config/auth/SecurityConfig.java
+++ airbnbClone/src/main/java/dbDive/airbnbClone/config/auth/SecurityConfig.java
@@ -15,6 +15,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -31,11 +34,25 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .authenticationProvider(authenticationProvider())
+                .addFilter(corsFilter())
                 .exceptionHandling().authenticationEntryPoint(customAuthenticationEntryPoint).and()
                 .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
                         .requestMatchers("/api/auth/**").authenticated()
                         .anyRequest().permitAll())
                 .build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // 내 서버가 응답할 때, json을 자바스크립트에서 처리할 수 있게 할지를 설정
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedHeader("*"); // 모든 header에 응답 허용
+        config.addAllowedMethod("*"); //모든 method 요청을 허용
+        source.registerCorsConfiguration("/api/**", config);
+        return new CorsFilter(source);
     }
 
     @Bean
